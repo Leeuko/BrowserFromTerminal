38e25d2504cb99eb56ad24424e81b2b4944fe4bd
Liuda <liudmilacosetova@gmail.com>
2020-11-26 19:02:14 +0200

    added options

--- src/main/java/DriverFactory.java
+++ src/main/java/DriverFactory.java
@@ -2,19 +2,27 @@ import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DriverFactory {
 
-    public static WebDriver getDriver(BrowserType browserType){
+    public static WebDriver create (BrowserType browserType, ArrayList<String>... listOptions){
+
         switch(browserType) {
             case CHROME:
                 WebDriverManager.chromedriver().setup();
                 return new ChromeDriver();
             case FIREFOX:
                 WebDriverManager.firefoxdriver().setup();
-                return new FirefoxDriver();
+                FirefoxOptions options = new FirefoxOptions()
+                        .addArguments(Arrays.toString(listOptions));
+                return new FirefoxDriver(options);
             default:
                 throw new IllegalArgumentException("Configuration for browser " + browserType + " has not been created");
         }
     }
+
 }
