13a15f3d912b183055795877ef9675c20372ef6e
Liuda <liudmilacosetova@gmail.com>
2020-12-10 12:51:09 +0200

    added @Options and two methods create

--- src/main/java/DriverFactory.java
+++ src/main/java/DriverFactory.java
@@ -1,15 +1,15 @@
 import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class DriverFactory {
 
-    public static WebDriver create (BrowserType browserType, ArrayList<String>... listOptions){
+    public static WebDriver create (BrowserType browserType){
 
         switch(browserType) {
             case CHROME:
@@ -17,9 +17,26 @@ public class DriverFactory {
                 return new ChromeDriver();
             case FIREFOX:
                 WebDriverManager.firefoxdriver().setup();
-                FirefoxOptions options = new FirefoxOptions()
-                        .addArguments(Arrays.toString(listOptions));
-                return new FirefoxDriver(options);
+                return new FirefoxDriver();
+            default:
+                throw new IllegalArgumentException("Configuration for browser " + browserType + " has not been created");
+        }
+    }
+
+    public static WebDriver create (BrowserType browserType, ArrayList<String> listOptions){
+
+        switch(browserType) {
+            case CHROME:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options1 = new ChromeOptions()
+                        .addArguments(listOptions);
+                return new ChromeDriver(options1);
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions options2 = new FirefoxOptions()
+                        .addArguments(listOptions)
+                        .addPreference("browser.startup.page", 1);
+                return new FirefoxDriver(options2);
             default:
                 throw new IllegalArgumentException("Configuration for browser " + browserType + " has not been created");
         }
--- src/test/java/ParamsTest.java
+++ src/test/java/ParamsTest.java
@@ -2,6 +2,7 @@ import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import config.ServerConfig;
 import org.aeonbits.owner.ConfigFactory;
@@ -16,15 +17,15 @@ public class ParamsTest {
     private WebDriver driver;
     private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
     private Logger logger = LogManager.getLogger(ParamsTest.class);
-    ArrayList<String> list = new ArrayList<>(Arrays.asList("start-maximized", "--headless"));
+    ArrayList<String> list2 = new ArrayList<>(Arrays.asList("browser.startup.homepage", "https://www.google.co.uk"));
 
     @BeforeTest
     @Parameters("browser")
-    public void Params(String browser) {
+    public void Params(@Optional("firefox") String browser) {
         browser = browser.toLowerCase();
         switch (browser) {
             case "firefox":
-                driver = DriverFactory.create(BrowserType.FIREFOX,list);
+                driver = DriverFactory.create(BrowserType.FIREFOX,list2);
                 driver.get(cfg.url());
                 break;
             case "chrome":
